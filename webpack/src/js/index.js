52b320abe680f5d2e4a6f2e301797320c35e60c2
flareg255 <flare.g255@gmail.com>
2024-10-20 21:44:48 +0900

    テクスチャの取り回し

--- webpack/src/js/index.js
+++ webpack/src/js/index.js
@@ -2,8 +2,26 @@ import "../css/style.css";
 import * as THREE from 'three'
 
 console.log(THREE);
+const loadingManager = new THREE.LoadingManager();
+loadingManager.onStart = (url, itemsLoaded, itemsTotal) => {
+    console.log( 'Started loading file: ' + url + '\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+};
+loadingManager.onLoad = () => {
+    console.log( 'Loading complete!');
+};
+loadingManager.onProgress = (url, itemsLoaded, itemsTotal) => {
+    console.log( url );
+    console.log( 'Loading file: ' + url + '\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+};
+loadingManager.onError = () => {
+    console.log("Error !");
+};
 
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const texture = textureLoader.load('img/map1.png');
 const scene = new THREE.Scene();
+
+
 
 // const geometery = new THREE.BoxGeometry(1,1,1, 2,2,2);
 // const geometery = new THREE.BufferGeometry();
@@ -17,8 +35,9 @@ const scene = new THREE.Scene();
 // const positionsAttribute = new THREE.BufferAttribute(verticeArray, 3);
 // geometery.setAttribute('position', positionsAttribute);
 // console.log(geometery);
-const geometery = new THREE.PlaneGeometry(1,1);
-const material = new THREE.MeshBasicMaterial({color:'aqua', wireframe:true});
+const geometery = new THREE.BoxGeometry(1,1,1);
+// const material = new THREE.MeshBasicMaterial({color:'aqua'});
+const material = new THREE.MeshBasicMaterial({map: texture});
 const mesh = new THREE.Mesh(geometery, material);
 
 scene.add(mesh);
@@ -39,14 +58,14 @@ rederer.setSize(aspect.width, aspect.height);
 
 const clock = new THREE.Clock();
 rederer.render(scene, camera);
-// const animate = () => {
-//     const elapsedTime = clock.getElapsedTime();
-//     console.log(elapsedTime);
-//     mesh.rotation.y += elapsedTime * Math.PI * 2;
+const animate = () => {
+    const elapsedTime = clock.getElapsedTime();
+    console.log(elapsedTime);
+    mesh.rotation.y += elapsedTime * Math.PI * 2 / 1000;
 
-//     rederer.render(scene, camera);
+    rederer.render(scene, camera);
 
-//     window.requestAnimationFrame(animate);
-// };
+    window.requestAnimationFrame(animate);
+};
 
-// animate();
+animate();
